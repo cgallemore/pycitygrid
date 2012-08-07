89d06728aba8457e09f1100f3c24a0200a301aa1
Chad Gallemore <cgallemore@gmail.com>
2012-08-06 23:24:39 -0400

    Used the api and made a successful request to city grid

--- citygrid/api.py
+++ citygrid/api.py
@@ -1,5 +1,7 @@
-from citygrid import CityGridException
+import json
+from citygrid.models import SearchResult
 import requests
+from citygrid import CityGridException
 
 class CityGrid(object):
     endpoint = 'http://api.citygridmedia.com/content/places/v2'
@@ -9,24 +11,24 @@ class CityGrid(object):
         self.publisher = publisher
 
     def _get(self, url, params):
-        return requests.get(url, params=params)
+        params = dict(format='json', publisher=self.publisher, **params)
+        _r = requests.get(url, params=params)
+        return SearchResult(json.loads(_r.text))
 
 class Places(CityGrid):
     def __init__(self, publisher):
         super(Places, self).__init__(publisher)
-        self.where_endpoint = '%s/search/where'
-        self.latlon_endpoint = '%s/searh/latlon'
+        self.where_endpoint = '%s/search/where' % self.endpoint
+        self.latlon_endpoint = '%s/searh/latlon' % self.endpoint
 
-    def where(self, where, format_='json', **kwargs):
+    def where(self, where, **kwargs):
+        if 'type' not in kwargs and 'what' not in kwargs:
+            raise CityGridException('Must provide one of type_ or what.')
+
+        return self._get(self.where_endpoint, dict(where=where, **kwargs))
+
+    def latlon(self, lat, long, **kwargs):
         if 'type' or 'what' not in kwargs:
             raise CityGridException('Must provide one of type_ or what.')
 
-        return self._get(self.where_endpoint, dict(where=where, format=format_,
-                                                   **kwargs))
-
-    def latlon(self, lat, long, format_='json', **kwargs):
-        if 'type' or 'what' not in kwargs:
-            raise CityGridException('Must provide one of type_ or what.')
-
-        return self._get(self.latlon_endpoint, dict(lat=lat, lon=long,
-                                                    format=format_, **kwargs))
+        return self._get(self.latlon_endpoint, dict(lat=lat, lon=long, **kwargs))
